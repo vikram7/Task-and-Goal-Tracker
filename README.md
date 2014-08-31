664d3c802c0ad40f186ad6de5c2d3791938e7244
Vikram Ramakrishnan <lord.ezar@gmail.com>
2014-08-31 12:40:29 -0400

    added deleted tasks feature where user can see old tasks; required adding a table column called -persists- set to true or false depending on whether the task is being currently considered or deleted

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 This is a simple task list and goal tracker I wrote in Ruby on Sinatra with a PostgreSQL database. The front-end uses Foundation. It allows a user to create tasks that he or she wants to complete by a certain day of the week or those that can be categorized as "short term" (something a user wants completed soon, but doesn't have a specific target date) and "long term" (these are typically long term goals like "write a science fiction novel" or "become Prime Minister")
 
+Here are a list of items that still need to be worked on:
 August 29, 2014:
 - Sanitize inputs
+- Add javascript for better deletion / updating features
 
 August 27, 2014:
-
-Here are a list of items that still need to be worked on:
 - hide checkboxes
 - create database of deleted items so the user can see what has been completed
 - fix front end to fill screen
@@ -15,3 +15,4 @@ Here are a list of items that still need to be worked on:
 
 Some nice to haves:
 - ability to text a task or pull tasks onto your phone
+- statistics (completed tasks vs uncompleted; how many are repeated)
--- server.rb
+++ server.rb
@@ -25,84 +25,95 @@ get '/' do
   dl_short_term = 'Short Term'
   dl_long_term = 'Long Term'
 
-  sql = "SELECT id, name FROM things_to_do WHERE deadline = $1"
+
+  sql = "SELECT id, name, persists FROM things_to_do WHERE deadline = $1 AND persists = $2"
     @sql_monday = db_connection do |db|
-      db.exec(sql,[dl_monday])
+      db.exec(sql,[dl_monday, true])
     end
 
-  sql = "SELECT id, name FROM things_to_do WHERE deadline = $1"
+  sql = "SELECT id, name, persists FROM things_to_do WHERE deadline = $1 AND persists = $2"
     @sql_tuesday = db_connection do |db|
-      db.exec(sql,[dl_tuesday])
+      db.exec(sql,[dl_tuesday, true])
     end
 
-  sql = "SELECT id, name FROM things_to_do WHERE deadline = $1"
+  sql = "SELECT id, name, persists FROM things_to_do WHERE deadline = $1 AND persists = $2"
     @sql_wednesday = db_connection do |db|
-      db.exec(sql,[dl_wednesday])
+      db.exec(sql,[dl_wednesday, true])
     end
 
-  sql = "SELECT id, name FROM things_to_do WHERE deadline = $1"
+  sql = "SELECT id, name, persists FROM things_to_do WHERE deadline = $1 AND persists = $2"
   @sql_thursday = db_connection do |db|
-    db.exec(sql,[dl_thursday])
+    db.exec(sql,[dl_thursday, true])
   end
 
-  sql = "SELECT id, name FROM things_to_do WHERE deadline = $1"
+  sql = "SELECT id, name, persists FROM things_to_do WHERE deadline = $1 AND persists = $2"
   @sql_friday = db_connection do |db|
-    db.exec(sql,[dl_friday])
+    db.exec(sql,[dl_friday, true])
   end
 
-  sql = "SELECT id, name FROM things_to_do WHERE deadline = $1"
+  sql = "SELECT id, name, persists FROM things_to_do WHERE deadline = $1 AND persists = $2"
   @sql_saturday = db_connection do |db|
-    db.exec(sql,[dl_saturday])
+    db.exec(sql,[dl_saturday, true])
   end
 
-  sql = "SELECT id, name FROM things_to_do WHERE deadline = $1"
+  sql = "SELECT id, name, persists FROM things_to_do WHERE deadline = $1 AND persists = $2"
   @sql_sunday = db_connection do |db|
-    db.exec(sql,[dl_sunday])
+    db.exec(sql,[dl_sunday, true])
   end
 
-  sql = "SELECT id, name FROM things_to_do WHERE deadline = $1"
+  sql = "SELECT id, name, persists FROM things_to_do WHERE deadline = $1 AND persists = $2"
   @sql_short_term = db_connection do |db|
-    db.exec(sql,[dl_short_term])
+    db.exec(sql,[dl_short_term, true])
   end
 
-  sql = "SELECT id, name FROM things_to_do WHERE deadline = $1"
+  sql = "SELECT id, name, persists FROM things_to_do WHERE deadline = $1 AND persists = $2"
   @sql_long_term = db_connection do |db|
-    db.exec(sql,[dl_long_term])
+    db.exec(sql,[dl_long_term, true])
+  end
+
+  sql = "SELECT id, name, persists FROM things_to_do WHERE persists = $1"
+  @sql_deleted = db_connection do |db|
+    db.exec(sql,[false])
   end
 
   erb :main
 end
 
-
 post '/take_a_task' do
-
-  @task = params["task"]
+  @task = params["task"].split(';')
   @deadline = params["deadline"]
-
-  sql = "INSERT INTO things_to_do (name, deadline) VALUES ($1, $2)"
-  db_connection do |db|
-    db.exec(sql, [@task,@deadline])
+  @task.each do |each_task|
+    sql = "INSERT INTO things_to_do (name, deadline, persists) VALUES ($1, $2, $3)"
+    db_connection do |db|
+      db.exec(sql, [each_task.lstrip, @deadline, true])
+    end
   end
-
   redirect '/'
-
 end
 
 post '/delete_tasks' do
-
   array_of_ids = params.keys
-
   array_of_ids.each do |x|
-
-    sql = "DELETE FROM things_to_do WHERE id = $1"
-      db_connection do |db|
+    sql = "UPDATE things_to_do
+    SET persists = false
+    where id = $1"
+    db_connection do |db|
       db.exec(sql, [x])
     end
-
   end
-
   redirect '/'
-
 end
+
+post '/permanently_delete' do
+  array_of_ids = params.keys
+  array_of_ids.each do |x|
+    sql = "DELETE FROM things_to_do WHERE id = $1"
+    db_connection do |db|
+      db.exec(sql, [x])
+    end
+  end
+  redirect '/'
+end
+
 
 #do a count to see how mayn you've completed
